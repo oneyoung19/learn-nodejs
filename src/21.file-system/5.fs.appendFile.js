7b44fd3b0716eac2a6a9a1753f16ebc54ce8d2ad
yangxiaoping <2878847069@qq.com>
2024-06-16 20:14:51 +0800

    feat: fs.appendFile

---
+++ src/21.file-system/5.fs.appendFile.js
@@ -0,0 +1,9 @@
+/*
+fs.appendFile()向目标文件中追加文本内容 
+如果目标文件不存在的话 会自动创建
+*/
+const path = require('node:path')
+const fs = require('node:fs')
+
+const result = fs.appendFileSync(path.resolve(__dirname, './test.txt'), 'Hello World\n')
+console.log(result)
