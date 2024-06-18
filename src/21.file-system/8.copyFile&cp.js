1f413de9c675c74189eaf8e85657947cd7c68427
yangxiaoping <2878847069@qq.com>
2024-06-18 22:22:46 +0800

    feat: copyFile&cp

---
+++ src/21.file-system/8.copyFile&cp.js
@@ -0,0 +1,9 @@
+const path = require('node:path')
+const fs = require('node:fs')
+
+// 复制某文件到新文件 如果 `dest` 已存在，则操作会覆盖
+// fs.copyFileSync(src,dest[,mode])
+
+// 复制目录到新目录
+// fs.cpSync(src,dest[,options])
+
