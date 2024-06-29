a6791fef1450662862a6c70ebc45dd8c7566d3f3
yangxiaoping <2878847069@qq.com>
2024-06-29 11:59:26 +0800

    feat: 添加sandpack-vue3

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>App</p>
+    <Sandpack template="vue3" />
+  </div>
+</template>
+
+<script>
+import { Sandpack } from 'sandpack-vue3'
+
+export default {
+  components: {
+    Sandpack
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped lang="less">
+</style>
