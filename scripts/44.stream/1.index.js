e5fb480f8ea6ccc05b500c07053d33f175360edc
yangxiaoping <2878847069@qq.com>
2024-07-19 20:18:50 +0800

    feat: 添加stream大纲

--- scripts/44.stream/1.index.js
+++ scripts/44.stream/1.index.js
@@ -1,5 +1,7 @@
 /*
 [美团Node.js Stream - 基础篇](https://tech.meituan.com/2016/07/08/stream-basics.html)
+[一文搞定 Node.js 流 （Stream）](https://juejin.cn/post/6854573219060400141)
+[Nodejs基础：stream模块入门介绍与使用——程序员小卡](https://www.cnblogs.com/chyingp/p/nodejs-learning-stream.html)
 
 在构建较复杂的系统时，通常将其拆解为功能独立的若干部分。这些部分的接口遵循一定的规范，通过某种方式相连，以共同完成较复杂的任务。
 
@@ -7,13 +9,24 @@
 
 在Node.js中，内置的Stream模块也实现了类似功能，各部分通过.pipe()连接。
 
-stream种类分为以下四种：
+1. stream种类分为以下四种：
 const Stream = require('stream')
 
+// 可读流
 const Readable = Stream.Readable
+// 可写流
 const Writable = Stream.Writable
+// 双工（可读可写流）
 const Duplex = Stream.Duplex
+// 转换（可读可写流 且支持transform转换）
 const Transform = Stream.Transform
+
+2. pipe()
+
+3. zip压缩
+
+4. stream数据积压
+
 */
 
 const path = require('node:path')
