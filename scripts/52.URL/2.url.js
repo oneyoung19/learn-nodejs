d89e2b059a95640f92e66fb06163931f56ccae34
yangxiaoping <2878847069@qq.com>
2024-07-28 21:46:06 +0800

    feat: url

---
+++ scripts/52.URL/2.url.js
@@ -0,0 +1,26 @@
+const { URL, URLSearchParams } = require('url')
+
+const myURL = new URL('https://example.com:8080/path/name?query=string#hash')
+/*
+URL {
+  href: 'https://example.com:8080/path/name?query=string#hash',
+  origin: 'https://example.com:8080',
+  protocol: 'https:',
+  username: '',
+  password: '',
+  host: 'example.com:8080',
+  hostname: 'example.com',
+  port: '8080',
+  pathname: '/path/name',
+  search: '?query=string',
+  searchParams: URLSearchParams { 'query' => 'string' },
+  hash: '#hash'
+}
+*/
+console.log(myURL)
+
+// 可以直接修改 URL 对象的属性：
+myURL.pathname = '/new/path'
+myURL.search = '?new=query'
+console.log(myURL.href)  // 'https://example.com:8080/new/path?new=query#hash'
+
