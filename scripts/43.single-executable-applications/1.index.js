e5f90edc36198730abad9a317936709c6a23d20b
yangxiaoping <2878847069@qq.com>
2024-07-30 20:42:58 +0800

    feat: Single Executable Applications

---
+++ scripts/43.single-executable-applications/1.index.js
@@ -0,0 +1,10 @@
+/*
+“Single Executable Applications”是指“Node.js SEA（Single Executable Applications）”功能。
+
+这是一个相对较新的特性，旨在将Node.js应用程序打包成单个可执行文件，简化分发和部署过程。
+
+这个特性内置在Node.js中，无需依赖外部工具。
+
+具体操作可以参考：[Single Executable Applications](https://nodejs.org/docs/latest/api/single-executable-applications.html)
+
+*/
