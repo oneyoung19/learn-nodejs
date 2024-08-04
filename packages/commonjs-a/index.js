271f193ef5ba3ca8452abd2812d28e553ec7a54c
yangxiaoping <2878847069@qq.com>
2024-08-04 17:38:27 +0800

    feat: imports字段支持设置内部简化路径

--- packages/commonjs-a/index.js
+++ packages/commonjs-a/index.js
@@ -1,3 +1,15 @@
+/*
+imports 字段用于定义内部模块的导入映射。
+
+它允许你为内部模块设置别名或映射路径，从而简化模块的导入。
+
+**imports 字段的路径必须以 # 符号开头。**
+
+*/
+const libModule = require('#lib')
+
+console.log(`CommonJS builtins:`, libModule)
+
 module.exports = {
   message: 'CommonJS'
 }
--- packages/esm-a/index.js
+++ packages/esm-a/index.js
@@ -1,3 +1,6 @@
+import libMessage from '#lib'
+
+console.log(`ESM builtins:`, libModule)
 
 export default {
   message: 'ESM'
