39dc14393d5699267ffbc4f6e65da8dff22e3050
yangxiaoping <2878847069@qq.com>
2024-08-22 10:36:09 +0800

    feat: 静态资源托管

--- playground/1.vanilla/index.js
+++ playground/1.vanilla/index.js
@@ -1,7 +1,7 @@
 const http = require('node:http')
 const path = require('node:path')
 const fs = require('node:fs')
-const { getAllFilePaths } = require('../utils')
+// const { getAllFilePaths } = require('../utils')
 
 const { globSync } = require('glob')
 
@@ -10,13 +10,16 @@ const staticPath = path.resolve(__dirname, '../static')
 // const staticGlob = `${staticPath}/**/*`
 // const staticFiles = fs.readdirSync(staticPath, { recursive: true })
 // const staticFiles = globSync([staticGlob], { nodir: true })
-const staticFiles = globSync(['playground/static/**/*'], { nodir: true }).map(path => path.replace(/^playground\/static\//, ''))
+const staticFiles = globSync(['playground/static/**/*'], { nodir: true }).map(path => path.replace(/^playground\/static/, ''))
 console.log(staticFiles)
 // console.log(getAllFilePaths(staticPath))
 
 const server = http.createServer((req, res) => {
   const { url } = req
-  const relativePath = url.slice(1)
+  if (staticFiles.includes(url)) {
+    res.end(fs.readFileSync(path.resolve(staticPath, `.${url}`)))
+    return
+  }
   res.end('Vanilla')
 })
 
