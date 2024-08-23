927b74e2f126da2e184ec434f9fec13943c657b5
yangxiaoping <2878847069@qq.com>
2024-08-23 17:23:27 +0800

    feat: 添加router

---
+++ playground/views/vue2/pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Home</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped lang="less">
+</style>
---
+++ playground/views/vue2/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export function createRouter () {
+  return new Router({
+    mode: 'history',
+    routes: [
+      {
+        name: 'Home',
+        path: '/',
+        component: () => import('./pages/Home.vue')
+      }
+    ]
+  })
+}
