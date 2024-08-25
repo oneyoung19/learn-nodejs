e00c4e22ed90411114ad0132a6f8068e2cea9081
yangxiaoping <2878847069@qq.com>
2024-08-25 18:11:22 +0800

    feat: 将vue-server-renderer移动至server

--- playground/1.vanilla/index.js
+++ playground/1.vanilla/index.js
@@ -6,9 +6,17 @@ const { globSync } = require('glob')
 // const { getRouteTemplate } = require('../views/template/index')
 const { createApp } = require('../views/vue2/scripts/app')
 // const { createApp } = require('../views/vue2/app')
+const renderer = require('vue-server-renderer').createRenderer({
+  template: fs.readFileSync(path.resolve(__dirname, '../views/vue2/public/index.html'), 'utf-8')
+})
 
 const staticPath = path.resolve(__dirname, '../static')
 const staticFiles = globSync(['playground/static/**/*'], { nodir: true }).map(path => path.replace(/^playground\/static/, ''))
+
+const context = {
+  title: 'Vue SSR',
+  meta: '<meta name="description" content="Vue.js 服务端渲染"><meta name="keywords" content="Vue,SSR">'
+}
 
 const server = http.createServer(async (req, res) => {
   let { url } = req
@@ -20,7 +28,8 @@ const server = http.createServer(async (req, res) => {
     return
   }
   // const routeContent = getRouteTemplate(url.slice(1))
-  const routeContent = await createApp(req)
+  const app = createApp(req)
+  const routeContent = await renderer.renderToString(app, context)
   if (routeContent) {
     res.end(routeContent)
     return
--- playground/views/vue2/scripts/app.js
+++ playground/views/vue2/scripts/app.js
@@ -3,16 +3,8 @@ const path = require('node:path')
 const fs = require('node:fs')
 
 const Vue = require('vue')
-const renderer = require('vue-server-renderer').createRenderer({
-  template: fs.readFileSync(path.resolve(__dirname, '../public/index.html'), 'utf-8')
-})
 
-const config = {
-  title: 'Vue SSR',
-  meta: '<meta name="description" content="Vue.js 服务端渲染"><meta name="keywords" content="Vue,SSR">'
-}
-
-const createApp = async (context) => {
+const createApp = (context) => {
   const { url } = context
   const app = new Vue({
     data: {
@@ -37,10 +29,7 @@ const createApp = async (context) => {
       }
     }
   })
-  return await renderer.renderToString(app, {
-    ...config,
-    ...context
-  })
+  return app
 }
 
 module.exports = {
