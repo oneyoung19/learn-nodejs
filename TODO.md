1692aa83501e5866caeae8441e64f491b225b3c1
yangxiaoping <2878847069@qq.com>
2024-08-29 14:21:19 +0800

    chore: express router

--- TODO.md
+++ TODO.md
@@ -8,4 +8,8 @@
 - [ ] （如果集群存在，那么在负载均衡的情况下，如何同步登录信息？）
 - [ ] TODO:?PM2为什么也能执行python脚本？除此以外，还有哪些可执行脚本？
 
+- [ ] `express` 洋葱模型
+- [ ] 日志记录
+- [ ] 错误处理
+
 [nodejs-learning-guide](https://github.com/oneyoung19/fork-nodejs-learning-guide)
--- frame/express/app.js
+++ frame/express/app.js
@@ -1,3 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const homeRouter = require('./router')
+const userRouter = require('./router/user')
+app.use('/', homeRouter)
+app.use('/user', userRouter)
+
+// 静态资源托管
+app.use('/static', express.static('public'))
+
+app.get('/', (req, res) => {
+  res.send('Got a GET request')
+})
+
+app.post('/', (req, res) => {
+  res.send('Got a POST request')
+})
+
+app.listen(port, () => {
+  console.log(`Example app running on http://127.0.0.1:${port}`)
+})
+
 /*
 https://expressjs.com/
 
