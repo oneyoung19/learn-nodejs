f2cd8eb7966892736bca32ab10584be582d130f3
yangxiaoping <2878847069@qq.com>
2024-09-03 21:03:46 +0800

    chore: express morgan

--- docs/frame/express.md
+++ docs/frame/express.md
@@ -157,6 +157,28 @@ app.use('/', (req, res, next) => {
 
 ## 9.日志记录
 
+`node-express-logger`
+
+```js
+// 创建日志文件写入流
+const fs = require('node:fs')
+const path = require('node:path')
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'logs/access.log'), { flags: 'a' })
+const errorLogStream = fs.createWriteStream(path.join(__dirname, 'logs/error.log'), { flags: 'a' })
+
+// combined > common > short > dev >= short
+app.use(morgan('combined', {
+  stream: accessLogStream, // 指定日志的输出流。默认是 process.stdout（控制台输出）
+  skip: (req, res) => res.statusCode >= 400, // 跳过特定条件下的日志记录
+  immediate: false // 立即记录日志，通常日志是在响应发送后记录
+}))
+app.use(morgan('combined', {
+  stream: errorLogStream,
+  skip: (req, res) => res.statusCode < 400,
+  immediate: false
+}))
+```
+
 ## 10.错误处理
 
 ## 11.debug
--- frame/express/app.js
+++ frame/express/app.js
@@ -2,12 +2,31 @@ const path = require('node:path')
 const express = require('express')
 const app = express()
 const session = require('express-session')
+const morgan = require('morgan')
 const port = 3000
 
 const homeRouter = require('./router')
 const userRouter = require('./router/user')
 const ajaxRouter = require('./router/ajax')
 const sessionRouter = require('./router/session')
+
+// 创建日志文件写入流
+const fs = require('node:fs')
+// const path = require('node:path')
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'logs/access.log'), { flags: 'a' })
+const errorLogStream = fs.createWriteStream(path.join(__dirname, 'logs/error.log'), { flags: 'a' })
+
+// combined > common > short > dev >= short
+app.use(morgan('combined', {
+  stream: accessLogStream, // 指定日志的输出流。默认是 process.stdout（控制台输出）
+  skip: (req, res) => res.statusCode >= 400, // 跳过特定条件下的日志记录
+  immediate: false // 立即记录日志，通常日志是在响应发送后记录
+}))
+app.use(morgan('combined', {
+  stream: errorLogStream,
+  skip: (req, res) => res.statusCode < 400,
+  immediate: false
+}))
 
 app.use(session({
   name: 'express-session-cookie',
